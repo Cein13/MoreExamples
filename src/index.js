1c0dde4bd23f031b06e45b6b80da300b62f667e0
kristina_cein <kristina.nesterova@hotmail.co.uk>
2020-10-14 12:28:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'Kristina',
+     age: 27
+};
+
+console.log(person);
+
+let selectedColors = ['black hair', 'brown eyes', 'red nose', 'green cheeks'];
+console.log(selectedColors);
+
+
+function greet (name, lastName, occupation){
+    console.log('Hey yo ' + name + ' ' + lastName + ' ' + occupation);
+}
+greet('Nicola', 'Lasdinene', 'developer');
